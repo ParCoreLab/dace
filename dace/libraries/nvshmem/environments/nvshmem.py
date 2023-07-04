d7cc90acd6a74a810655fef9a82031760e60a4c7
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2023-07-04 17:11:32 +0300

    Added NVSHMEM environment and init code

--- dace/libraries/nvshmem/environments/nvshmem.py
+++ dace/libraries/nvshmem/environments/nvshmem.py
@@ -4,18 +4,48 @@ import dace.library
 
 @dace.library.environment
 class NVSHMEM:
-    pass
-    # cmake_minimum_version = "3.6"
-    # cmake_packages = ["MPI"]
-    # cmake_files = []
-    # cmake_variables = {}
-    # cmake_includes = []
-    # cmake_libraries = ["${MPI_CXX_LIBRARIES}"]
-    # cmake_compile_flags = ["-I${MPI_CXX_HEADER_DIR}"]
-    # cmake_link_flags = ["${MPI_LINKER_FLAGS}"]
-    #
+    cmake_minimum_version = "3.11"
+    cmake_packages = ["MPI", "NVSHMEM"]
+    cmake_files = []
+    cmake_variables = {}
+    cmake_includes = ["/home/jbaydamirli21/nvshmem/include"]
+    cmake_libraries = ["${MPI_CXX_LIBRARIES}"]
+    cmake_compile_flags = ["-I${MPI_CXX_HEADER_DIR}"]
+    # cmake_link_flags = ["-lnvshmem_host -lnvshmem_device", "${MPI_LINKER_FLAGS}"]
+    cmake_link_flags = ["${MPI_LINKER_FLAGS}", "-L/home/jbaydamirli21/nvshmem/lib/", "-lnvshmem_host -lnvshmem_device"]
+
     # headers = ["mpi.h"]
-    # state_fields = []
-    # init_code = "int t; MPI_Initialized(&t);  if (!t) MPI_Init(NULL, NULL);"
-    # finalize_code = "// MPI_Finalize();"  # actually if we finalize in the dace program we break pytest :)
-    # dependencies = []
+    # headers = ["mpi.h", 'nvshmem.h', 'nvshmemx.h']
+    headers = {
+        'frame': ["mpi.h", "nvshmem.h", "nvshmemx.h"],
+        'cuda': ["mpi.h", "nvshmem.h", "nvshmemx.h"]
+    }
+
+    state_fields = []
+
+    # This might break? There's a check to see if MPI is initialized
+    # but I don't have the same for NVSHMEM
+    init_code = """
+        int t;
+        MPI_Initialized(&t);
+        if (!t) {
+            MPI_Init(NULL, NULL);
+        } else {
+            printf("MPI is initialized already\\n");
+        }
+
+        int rank, ndevices;
+
+        nvshmemx_init_attr_t attr;
+        MPI_Comm comm = MPI_COMM_WORLD;
+        attr.mpi_comm = &comm;
+
+        cudaGetDeviceCount(&ndevices);
+        cudaSetDevice(rank % ndevices);
+        nvshmemx_init_attr(NVSHMEMX_INIT_WITH_MPI_COMM, &attr);
+    """
+    finalize_code = """
+        //nvshmem_finalize();
+        //MPI_Finalize();
+    """  # actually if we finalize in the dace program we break pytest :) ok
+    dependencies = []
