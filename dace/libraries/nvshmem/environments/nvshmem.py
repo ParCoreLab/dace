840d6290c447a589d5913423962c3d15323c1276
Javid Baydamirli <jbaydamirli21@ku.edu.tr>
2023-07-20 16:14:17 +0300

    Temporary patches to get NVSHMEM compiling and linking

--- dace/libraries/nvshmem/environments/nvshmem.py
+++ dace/libraries/nvshmem/environments/nvshmem.py
@@ -1,18 +1,25 @@
+
 # Copyright 2019-2021 ETH Zurich and the DaCe authors. All rights reserved.
 import dace.library
+
+# NVSHMEM_HOME = "/home/jbaydamirli21/spack/opt/spack/linux-ubuntu20.04-zen2/gcc-11.1.0/nvshmem-2.7.0-6-svccom42hd6t6fmfru3txongtfpvuynm/"
+# MPI_HOME = "/home/jbaydamirli21/spack/opt/spack/linux-ubuntu20.04-zen2/gcc-11.1.0/openmpi-4.1.4-cgf2kyjuumewmbove7jagikdbpo42s6q/"
+
+# NVSHMEM_HOME = "/usr/local/nvshmem/"
 
 
 @dace.library.environment
 class NVSHMEM:
     cmake_minimum_version = "3.11"
-    cmake_packages = ["MPI", "NVSHMEM"]
+    cmake_packages = []
     cmake_files = []
     cmake_variables = {}
-    cmake_includes = ["/home/jbaydamirli21/nvshmem/include"]
-    cmake_libraries = ["${MPI_CXX_LIBRARIES}"]
-    cmake_compile_flags = ["-I${MPI_CXX_HEADER_DIR}"]
+    cmake_includes = []
+    cmake_libraries = []
+    cmake_compile_flags = []
     # cmake_link_flags = ["-lnvshmem_host -lnvshmem_device", "${MPI_LINKER_FLAGS}"]
-    cmake_link_flags = ["${MPI_LINKER_FLAGS}", "-L/home/jbaydamirli21/nvshmem/lib/", "-lnvshmem_host -lnvshmem_device"]
+    # cmake_link_flags = [f"-L{NVSHMEM_HOME}/lib", f"-L{MPI_HOME}/lib", "-lmpi", "-lnvshmem", "-lnvshmem_host", "-lnvshmem_device", "-lcudart", "-lcudadevrt"]
+    cmake_link_flags = []
 
     # headers = ["mpi.h"]
     # headers = ["mpi.h", 'nvshmem.h', 'nvshmemx.h']
@@ -25,6 +32,9 @@ class NVSHMEM:
 
     # This might break? There's a check to see if MPI is initialized
     # but I don't have the same for NVSHMEM
+
+    # init_code = ""
+
     init_code = """
         int t;
         MPI_Initialized(&t);
@@ -44,8 +54,11 @@ class NVSHMEM:
         cudaSetDevice(rank % ndevices);
         nvshmemx_init_attr(NVSHMEMX_INIT_WITH_MPI_COMM, &attr);
     """
-    finalize_code = """
-        //nvshmem_finalize();
-        //MPI_Finalize();
-    """  # actually if we finalize in the dace program we break pytest :) ok
+    # finalize_code = """
+    #     //nvshmem_finalize();
+    #     //MPI_Finalize();
+    # """  # actually if we finalize in the dace program we break pytest :) ok
+
+    finalize_code = ""
+
     dependencies = []
